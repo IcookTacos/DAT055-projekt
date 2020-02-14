123ffed64b1895220c5b4ebd94d4490210e009a5
IcookTacos <zeidlitzerik@gmail.com>
2020-02-14 15:16:52 +0100

    JadajadaServer_v1.1
    - Created new GUI for the server
    - Trimmed down server latency, now processes messages faster
    - Light bugfixes

--- JadajadaServer/src/handlerPackage/HandlerInterface.java
+++ JadajadaServer/src/handlerPackage/HandlerInterface.java
@@ -1,9 +1,8 @@
 package handlerPackage;
 
-
 public interface HandlerInterface {
 	
 	public void tellEveryone(String string1, String string2);
 	public void run();
-	public void sendMsgHistory();
+
 }
